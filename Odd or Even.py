5d831761fbb3a8774608c3ae81b87d6bdabf0c95
Juma <109604462+ClintonJuma@users.noreply.github.com>
2022-08-27 03:59:57 -0700

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,6 @@
+number=int(input("Input a random number: "))
+rem=number%2
+if rem>0:
+    print("You picked an odd number")
+else:
+    print("You picked an even number")
---
+++ character input.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+year=2022
+gap=100-age
+print(name+" you will turn 100 years at "+ str(year+gap))
+
